50c362c9a311fac9bef817cfa9effcf6d91cc2a5
Rapid-SBS <48488155+Rapid-SBS@users.noreply.github.com>
2019-03-26 09:55:11 -0700

    content.php stripped down

--- content.php
+++ content.php
@@ -2,7 +2,7 @@
 	<h2 class="blog-post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 	<p class="blog-post-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
 
- <?php the_excerpt(); ?>
+<?php the_excerpt(); ?>
 
 <a href="<?php comments_link(); ?>">
 	<?php
