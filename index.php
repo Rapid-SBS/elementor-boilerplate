b80d468689c572ee06cc5114048a9f263fd7d4a9
Rapid-SBS <48488155+Rapid-SBS@users.noreply.github.com>
2019-03-26 08:48:47 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,5 @@
 <!-- index.php -->
-<div class="page-min-width">
 <?php get_header(); ?>
-
-    <div class="container">
-	<div class="row">
-		<div class="col-sm-8 blog-main">
-
-		</div>	<!-- /.blog-main -->
-		<?php get_sidebar(); ?>
-	</div><!-- /.row -->
-    </div><!-- /.container-->
-
-<!-- ===[ contact-ribbon.php ]=== -->
-
-<?php get_template_part( 'theme-components/contact-ribbon' ); ?>
-
-<!-- ==========[ footer.php ]========== -->
-
+	<?php get_sidebar(); ?>
+	<?php get_template_part( 'theme-components/contact-ribbon' ); ?>
 <?php get_footer(); ?>
-<div class="page-min-width">
