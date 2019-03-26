130e7c64b0af73cb87522a0dd1dfadc1dfe83099
Rapid-SBS <48488155+Rapid-SBS@users.noreply.github.com>
2019-03-26 09:09:02 -0700

    Index.php stripped down

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!-- index.php -->
 <?php get_header(); ?>
 	<?php get_sidebar(); ?>
-	<?php get_template_part( 'theme-components/contact-ribbon' ); ?>
+	<!-- <?php get_template_part( '' , ''); ?> -->
 <?php get_footer(); ?>
