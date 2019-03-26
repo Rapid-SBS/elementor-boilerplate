9dd55ebfa58090f6230f83f3429734244e8714b0
Rapid-SBS <48488155+Rapid-SBS@users.noreply.github.com>
2019-03-26 09:54:38 -0700

    Update content.php

--- content.php
+++ content.php
@@ -1,4 +1,4 @@
-<div class="blog-post">
+<div id="blog-post">
 	<h2 class="blog-post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 	<p class="blog-post-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
 
@@ -8,5 +8,3 @@
 	<?php
 	printf( _nx( 'One Comment', '%1$s Comments', get_comments_number(), 'comments title', 'textdomain' ), number_format_i18n( 						get_comments_number() ) ); ?>
 </a>
-
-</div><!-- /.blog-post -->
